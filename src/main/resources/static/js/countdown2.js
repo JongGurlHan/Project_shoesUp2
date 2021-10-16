aa0d2bf69f156b931752f441f9e61725f044f4d5
JG HAN <gojkhan13@gmail.com>
2021-10-16 22:11:27 +0900

    1013 파일업로드 설정 변경

---
+++ src/main/resources/static/js/countdown2.js
@@ -0,0 +1,12 @@
+const timeH = document.querySelector('h1'); //h1 영역
+let timeSecond = 5;
+
+timeH.innerText ='${timeSecond}';
+
+
+// const countDown = setInterval(()=>{
+//   timeSecond--;
+//   timeH.innerHTML = '00:${timeSecond}';
+
+// }, 1000)
+
